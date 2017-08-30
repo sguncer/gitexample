010dcaf925e108c4e0c377455ca6b0cca24fd041
Selim <selim_guncer@yahoo.com>
2017-08-29 21:29:23 -0700

    dragonboard hello

---
+++ app/src/main/java/com/example/selim/gittest/GitexampleMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.selim.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitexampleMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gitexample_main);
+    }
+}
